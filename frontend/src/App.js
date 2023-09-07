e3df867b2aa87d2d9c8c019a2cd3a84b336c29a3
GSgiansen <giansenrockz@gmail.com>
2023-09-07 21:02:03 +0800

    Attemp to connect to supabase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vYWpvdmthZnZqY2FncmV6ZXdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwOTEyNDYsImV4cCI6MjAwOTY2NzI0Nn0.UHSA2iY2lxZoAWHfIl1MYfgfD_bEpyMX9w3KmB4W10w"
+const supabaseUrl = "https://ooajovkafvjcagrezewd.supabase.co"
+const supabaseKey = SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  async function getCountries() {
+    
+  let { data: booking, error } = await supabase
+  .from('booking')
+  .select('id')
+
+    setCountries(booking);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {countries.map((country) => (
+        <li key={country.id}>{country.id}</li>
+      ))}
+    </ul>
   );
 }
 
