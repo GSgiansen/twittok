b3b359059c69a3853cbcfd56e45d180b7e070f2b
GSgiansen <giansenrockz@gmail.com>
2023-09-09 20:49:51 +0800

    Integration with sentiment analysis

--- frontend/src/components/ui/reviews.tsx
+++ frontend/src/components/ui/reviews.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -9,10 +9,11 @@ import {
 } from "@/components/ui/dialog";
 import supabase from "@/supabaseClient";
 import { Textarea } from "./textarea";
-
-
+import { AuthContext } from "@/App";
+import Review from "./review";
 
 const Reviews = ({ productId }) => {
+  const authObject = useContext(AuthContext);
   const [reviews, setReviews] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
   const [review, setReview] = useState("");
@@ -30,35 +31,67 @@ const Reviews = ({ productId }) => {
   };
 
   const fetchReviews = async (productId) => {
-    console.log("fetching for ", productId)
+    console.log("fetching for ", productId);
     try {
       // Replace this with your actual API request or Supabase query
-      const {data: reviews, error} = await supabase.from("reviews").select("*").eq("product_id", productId);
+      const { data: reviews, error } = await supabase
+        .from("reviews")
+        .select("*")
+        .eq("product_id", productId);
       if (reviews) {
+        console.log("reviews is ", reviews)
         setReviews(reviews);
       } else {
-        console.error("Error fetching reviews:", error)
+        console.error("Error fetching reviews:", error);
       }
     } catch (error) {
       console.error("Error fetching reviews:", error);
     }
   };
+  const calculateAverageSentiment = () => {
+    if (reviews.length === 0) {
+      return 0;
+    }
+  
+    let sumSentiment = 0;
+    for (const review of reviews) {
+      sumSentiment += review.sentiment;
+    }
+  
+    return sumSentiment / reviews.length;
+  };
+  
+  // Calculate the average sentiment score
+  const averageSentiment = calculateAverageSentiment();
+  
 
   const handleSubmit = async () => {
-    console.log("session is ", session)
-    const num = Math.floor(Math.random() * 1000000000)
-    const {error} = await supabase.from('reviews').insert({ id:num, product_id: productId, description: review
-        , commenter: session.session.user.id
-    })
+    console.log("session is ", authObject);
+    const num = Math.floor(Math.random() * 1000000000);
+    const { error } = await supabase
+      .from("reviews")
+      .insert({
+        id: num,
+        product_id: productId,
+        description: review,
+        commenter: authObject.user.id,
+      });
     if (error) {
-        console.error("Error inserting review:", error);
-        }
-    else {
-        console.log("Review inserted successfully")
-        }
-    setReviews([...reviews, {id:num, product_id: productId, description: review}])
-    setReview("")
-  }
+      console.error("Error inserting review:", error);
+    } else {
+      console.log("Review inserted successfully");
+    }
+    // setReviews([
+    //   ...reviews,
+    //   { id: num, product_id: productId, description: review },
+    // ]);
+    setReview("");
+    setTimeout(() => {
+      fetchReviews(productId);
+    }, 6000);
+  };
+  var sum = 0;
+  let count = 0;
 
   return (
     <div>
@@ -68,30 +101,41 @@ const Reviews = ({ productId }) => {
           <DialogHeader>
             <DialogTitle>Reviews for Product</DialogTitle>
             <DialogDescription>
+              <Textarea
+                placeholder="What about the product ?"
+                className="m-2 w-full p-2 rounded-md mb-2 text-white"
+                value={review}
+                onChange={(e) => setReview(e.target.value)}
+              />
+              <button
+                className="button m-2"
+                type="button"
+                onClick={handleSubmit}
+              >
+                Submit
+              </button>
+              <div className="m-2 text-2xl font-bold">
+                Average sentiment score: {averageSentiment.toFixed(2)}
+              </div>
               {reviews.length === 0 ? (
-                <p>No reviews available.</p>
-              ) : (
-                <div>
-                    <Textarea
-                    placeholder="What about the product ?"
-                    className="m-2 w-full p-2 rounded-md mb-2 text-white"
-                    value={review}
-                    onChange={(e) => setReview(e.target.value)}
-                    />
-                    <button className="button m-2" type="button" onClick={handleSubmit}
-                    >
-                        Submit
-                    </button>
-
-                    <ul>
-                        {reviews.map((review) => (
-                        <li className="m-2" key={review.id}>{review.description}</li>
-                        ))}
-                    </ul>
+              <div className="m-2">No reviews available.</div>
+            ) : (
+              
+              reviews.map((review, index) => (
+                <Review
+                 className="m-2"
+                  key={index}
+                  id={review.id}
+                  description={review.description}
+                  sentimentScore={review.sentiment}
+                  productId={review.product_id}
+                />
+              ))
+            )
+            }
+            
 
 
-                </div>
-              )}
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
